059bb34c87dbd873b7bcd1bd7383ca2a0cf7079d
njcos <36767444+njcos@users.noreply.github.com>
2024-07-19 15:54:18 -0400

    stTIC

--- index.js
+++ index.js
@@ -3,19 +3,14 @@ import morgan from 'morgan';
 import cors from 'cors';
 import process from 'process';
 
-
-
-
 const app = express()
-
-
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 })
 
-app.use(express.json())
+app.use(express.static('dist'))
 app.use(cors())
 
 morgan.token('person', (req) => {
