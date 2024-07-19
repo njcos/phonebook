878e544797f7e5657fd5757b9bf8bcaac2127fcc
njcos <36767444+njcos@users.noreply.github.com>
2024-07-19 14:42:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ app.get('/api/info', (request,response) => {
 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
